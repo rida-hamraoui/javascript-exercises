d8fc7e2c63ff852a78344bc5a96104761f447a78
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-09 00:50:45 +0100

    12_findTheOldest: Exercice Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    let indexOld = 0;
+    let oldAge = 0;
 
+    for(let i = 0; i < persons.length ; i++){
+        if(!("yearOfDeath" in persons[i])){
+            const d = new Date();
+            if(d.getFullYear() - persons[i].yearOfBirth > oldAge){
+                indexOld = i;
+                oldAge = d.getFullYear() - persons[i].yearOfBirth;
+            }
+        }else{
+            if(persons[i].yearOfDeath - persons[i].yearOfBirth > oldAge){
+                indexOld = i;
+                oldAge = persons[i].yearOfDeath - persons[i].yearOfBirth;
+            }
+        }        
+    }
+    return persons[indexOld];
 };
 
 // Do not edit below this line
