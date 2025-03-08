04c00cab786ec77b2ddee341be3179e26134bff8
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 23:03:04 +0100

    07_tempConversion: Exercice Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(fahrenheitTem) {
+  return round(((fahrenheitTem - 32) * 5/9), 1) ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTem) {
+  return round((celsiusTem * 9/5 + 32), 1) ;
 };
 
 // Do not edit below this line
