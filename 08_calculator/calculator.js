bb895b94b49af423bfa57b5e3c7db950c19686a9
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 23:21:18 +0100

    08_calculator: Exercice Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	result = 0;
+	for(let i=0; i < arr.length ; i++){
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = 1;
+	for(let i=0; i < arr.length ; i++){
+    result *= arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	return base ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0) return 1;
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line
