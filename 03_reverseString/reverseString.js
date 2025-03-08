c0b244cb21a4fafeb49d8751f5b4bd36fd716533
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 13:06:19 +0100

    03_reverseString: Exercice Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringa) {
+    let result = "";
+    for(let j = stringa.length ; j >= 0; j--){
+        result += stringa.charAt(j);
+    }
+    return result;
 };
 
 // Do not edit below this line
