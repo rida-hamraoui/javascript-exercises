ca43717f4dc599e1d57eb0ea1a5187467971684a
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 13:32:23 +0100

    04_ramoveFromArray: Exercice complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToChange, ...toDelete) {
+    const result = [];
+    for(let i = 0; i < arrayToChange.length; i++){
+        if(toDelete.includes(arrayToChange[i])  ){
+            continue;
+        }else{
+            result.push(arrayToChange[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
