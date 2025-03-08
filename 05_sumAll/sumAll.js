a5aee3bb7a7d8e70d84473873033b6b810d8a2af
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 13:54:37 +0100

    05_sumAll: Exercice Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if( !Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return "ERROR";
+    let sum = 0;
 
+    if(start > end){
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
