71f6914241a5effbe38fdd85fd31ef5ac305a80e
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 23:57:35 +0100

    10_fibonacci: Exercice Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0){
+        return "OOPS"
+    }else if(index == 0){
+        return 0;
+    }else if(index == 1){
+        return 1;
+    }else{
+        let fib1 = 0;
+        let fib2 = 1;
+        for(let i = 1; i < index; i++){
+            let tmp = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = tmp;
+        }
+        return fib2;
+    }
 };
 
 // Do not edit below this line
