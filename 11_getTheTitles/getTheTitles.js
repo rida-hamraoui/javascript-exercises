cf79e9b4161e1b71af46316595ab9f1edbb97fc8
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-09 00:07:49 +0100

    11_getTheTitles: Exercice Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = [];
+    for(let i = 0; i < books.length; i++){
+        titles.push(books[i].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
