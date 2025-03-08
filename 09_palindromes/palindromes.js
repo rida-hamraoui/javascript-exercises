5c44e0c06d3b3712b6ff6791e9848e7c249c632a
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 23:38:45 +0100

    09_palindromes: Exercice Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "");
+    word = word.toLowerCase();
+    result = true;
+    for(let i = 0, j = word.length -1; i < j; i++, j--){
+        if(word.charAt(i) != word.charAt(j)) 
+            result = false;
+    }
+    return result;
 };
 
 // Do not edit below this line
