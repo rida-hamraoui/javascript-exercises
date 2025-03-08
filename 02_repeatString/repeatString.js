d9649ce4914cbb5dd5ece95a8949bb671bcf7f7d
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-08 12:53:47 +0100

    02_repeatString: Exercice Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(toBeRepeated, nTimes) {
+    if(nTimes < 0) return "ERROR";
+    let Repeated = '';
+    for(let i = 0; i <nTimes; i++){
+        Repeated = Repeated + toBeRepeated;
+    }
+    return Repeated;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
